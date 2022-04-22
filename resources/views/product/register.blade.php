aeac877ea16fad63183e510c39df8c4cbc6d2557
demekeyidege <demekeyideg2004@gmail,com>
2022-04-22 12:04:31 +0300

    commite

---
+++ resources/views/product/register.blade.php
@@ -0,0 +1,37 @@
+<html>
+    <header>
+        <title> ecommerce</title>
+</header>
+<body>
+<center>
+    <font color ="grean">
+    <h1> REGISTRATION PAGE</h2>
+</font>
+    <form>
+   
+        <table>
+            <tr>
+            <td>Name:</td>
+                <td> <input type="name" name="name" id="name"  placeholder="Enter name"></td>
+</tr>
+<tr>
+    <td>Unit:</td>
+        <td> <input type="unit" name="unit" id="unit" placeholder="Enter unit"> </td>
+</tr>
+<tr>
+    <td> Price:</td>
+        <td> <input type="price" name="price" id ="price" placeholder="Enter price"> </td>
+</tr>
+<tr>
+    <td>Quantity:</td>
+        <td><input type="quantity" name="quantity" id="quantity" placeholder="Enter quantity"> </td>
+        </tr>
+        
+    </table>
+    </form>
+    </center>
+    <center>
+    <input type="button" name="button" value="register">
+    </center>
+    </body>
+    </html>
